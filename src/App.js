89db58ef47e8e6cdeeecfe0c8e5d0ede3300415e
Wagner Alcantara Fernandez <92271202+Jp12wag@users.noreply.github.com>
2024-04-02 12:17:58 -0400

    PULL

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import VentanaIniciSesion from './componentes/Inicio';
-
+import Formulario from './componentes/Registro';
+import VentanaPrincipal from './componentes/Principal';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Importa Routes en lugar de Switch
+import Headers from './componentes/Header'
 function App() {
   return (
-    <div className="App">
-      <div className='contenedor-principal'>
-      <VentanaIniciSesion/>
+    <Router>
+      <Headers/>
+      <div className="App">
+        <div className='contenedor-principal'>
+          <Routes>
+            <Route path="/" element={<VentanaIniciSesion />} /> {/* Usa 'element' en lugar de 'component' */}
+            <Route path="/registro" element={<Formulario />} /> {/* Usa 'element' en lugar de 'component' */}
+            <Route path="/principal" element={<VentanaPrincipal/>} /> {/* Usa 'element' en lugar de 'component' */}
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
